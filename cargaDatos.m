f23a605e0f7869e507dfb28b07932f788302c820
emmanuel <eava745@gmail.com>
2016-11-24 08:41:38 -0500

    pequeña correccion

--- cargaDatos.m
+++ cargaDatos.m
@@ -5,7 +5,6 @@ a=readtable('hour.csv');
 b=a;
 b(:,[2,3,4,15,16])=[];
 DatosBalanceados=table2array(b);
-%DatosBalanceados = DatosBalanceados((1:1000),:);
 X=DatosBalanceados(:,(1:11));
 Y=DatosBalanceados(:,12);
 save('datos.mat');
